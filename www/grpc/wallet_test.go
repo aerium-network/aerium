dfe8be807be84be3502fa169ebad9bf73d761dc7
Laaaaksh <laksh.sadhwani07@gmail.com>
2025-10-19 10:33:46 +0530

    fix(wallet_test) - removing the not needed assert for wallet name

--- www/grpc/wallet_test.go
+++ www/grpc/wallet_test.go
@@ -312,8 +312,6 @@ func TestGetWalletInfo_DefaultFee(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.NotNil(t, res)
-
-	assert.Equal(t, walletName, res.WalletName)
 	// Default fee should be the vault default (0.01 AUM => 10_000_000 NanoAUM)
 	assert.EqualValues(t, int64(10_000_000), res.DefaultFee)
 }
