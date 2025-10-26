1a97ee1e934902351bb089ad1296863b2d4b50e8
Shravan A Y <shravanay205@gmail.com>
2025-10-26 20:35:07 +0530

    Fix issue gui not including daemon

--- .github/releasers/releaser_gui_centos.sh
+++ .github/releasers/releaser_gui_centos.sh
@@ -17,13 +17,16 @@ URL=$(echo `git remote show origin | grep "Fetch URL"` | awk '{printf $3}')
 CATEGORY="User Interface/Desktops"
 
 # Check the architecture
-ARC="$(uname -m)"
+ARC="aarch64"
 
 mkdir -p ${PACKAGE_DIR}
 
 echo "Building the GUI binaries for CentOS ${ARC} architecture"
 
 cd ${ROOT_DIR}
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/aerium-daemon ./cmd/daemon
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/aerium-wallet ./cmd/wallet
+CGO_ENABLED=0 go build -ldflags "-s -w" -trimpath -o ${BUILD_DIR}/aerium-shell ./cmd/shell
 go build -ldflags "-s -w" -trimpath -tags gtk -o ${BUILD_DIR}/aerium-gui ./cmd/gtk
 
 
@@ -36,18 +39,19 @@ fpm -s dir -t rpm \
 	-n "aerium-gui" \
 	-v "${VERSION}" \
 	-a "${ARC}" \
+	--depends "aerium-cli = ${VERSION}" \
 	--license "${LICENSE}" \
 	--maintainer "${MAINTAINER}" \
 	--description "${DESC}" \
-	--conflicts aerium-cli \
-	--replaces aerium-cli \
-	--provides aerium-cli \
 	--url "${URL}" \
 	--category "${CATEGORY}" \
 	--rpm-os linux \
+	"${BUILD_DIR}/aerium-daemon"=/usr/local/bin/aerium-daemon \
+	"${BUILD_DIR}/aerium-shell"=/usr/local/bin/aerium-shell \
+	"${BUILD_DIR}/aerium-wallet"=/usr/local/bin/aerium-wallet \
 	"${BUILD_DIR}/aerium-gui"=/usr/local/bin/aerium-gui \
 	"${ROOT_DIR}/.github/releasers/linux/aerium-gui.desktop"=/usr/share/applications/aerium-gui.desktop \
-	"${ROOT_DIR}/cmd/gtk/assets/images/logo.png"=/usr/share/icons/hicolor/256x256/apps/aerium.png
+	${ROOT_DIR}/cmd/gtk/assets/images/logo.png"=/usr/share/icons/hicolor/256x256/apps/aerium.png
 
 echo "RPM package built successfully for ${ARC} architecture"
 
